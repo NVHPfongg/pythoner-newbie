c79a1b6f5d966c39cf720f3ce44db266947e2b0e
NVHPfongg <haiifongg1310.9@gmail.com>
2025-12-20 10:26:11 +0700

    sea roma

--- hehe.py
+++ hehe.py
@@ -1,4 +1,14 @@
 print ("hello")
 age = input("age :")
-name = input("hay nhap ten cua ban")
+name = input("hay nhap ten cua ban :")
 print (f"chao {name}, ban {age} tuoi")
+# c1 de tinh hinh tron
+r = float(input("nhap ban kinh cua hing tron :"))
+chu_vi= 2* 3.14159 * r
+dien_tich=3.14159 **r
+print(f"chu vi hinh tron la: {chu_vi}")
+print(f"dien_tich hinh tron la: {dien_tich}")
+print(f"dap an cua cau 10+5 la : {10+5}")
+print(f"dap an cua cau 10-5 la : {10-5}")
+print(f"dap an cua cau 10*5 la : {10*5}")
+print(f"dap an cua cau 10/5 la : {10/5}")
